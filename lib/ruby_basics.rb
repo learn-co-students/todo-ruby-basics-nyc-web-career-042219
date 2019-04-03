eae1ff16266a363305ce8ecf7ba8c5e5ace451eb
Elma Rahman <elmarahman635@gmail.com>
2019-04-03 20:34:33 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,34 @@
 def division(num1, num2)
- 
+  36/6
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  "#{name}"
 end
 
-def argue
-  
+assign_variable("Elma")
+
+
+def argue(with="wrong!")
+puts "I am right and you are #{with}"
+with
 end
 
-def greeting
-
+def greeting(name, language)
+puts "Hey #{name}, heard you programmed in #{language}"
 end
 
-def return_a_value
-
+def return_a_value(behaivor="Nice")
+  puts "#{behaivor}"
+  behaivor
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(level="expert")
+  puts "#{level}"
+  level
 end
 
-def pizza_party
-
+def pizza_party(default="cheese")
+  puts "#{default}"
+  default
 end
